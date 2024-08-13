519b2b712319aaf0f7ec30ee9d6f6706a9e2f7eb
Jeremy Aboudan <aboudan.jeremy@gmail.com>
2024-08-13 16:04:18 +0200

    header

---
+++ src/Components/Header.jsx
@@ -0,0 +1,87 @@
+import React, { useRef, useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import Logo from "../img/Logo.png"
+import "../css/header.css"
+
+
+function Header() {
+
+    const [showSearch, setShowSearch] = useState(false);
+    let searchRef = useRef();
+
+    const [showLinks, setShowLinks] = useState(false);
+    let menuRef = useRef();
+
+    
+    useEffect(() => {
+
+        let closeSearch = (e) => {
+            if (!searchRef.current.contains(e.target)){
+                setShowSearch(false);
+            }
+        };
+        
+        let closeMenu = (e) => {
+            if (!menuRef.current.contains(e.target)){
+                setShowLinks(false);
+            }
+        };
+
+        window.addEventListener('resize', () => {
+            if(window.innerWidth > 719) {
+                setShowLinks(false);
+                setShowSearch(false);
+            }
+        })
+
+        document.addEventListener("mousedown", closeSearch);
+        document.addEventListener("mousedown", closeMenu);
+
+        return() => {
+            document.removeEventListener("mousedown", closeSearch);
+            document.removeEventListener("mousedown", closeMenu);
+        };
+
+    });
+    
+    
+    return(
+        <header>
+            <nav className="nav">
+                <div className="header-logo-div">
+                    <Link to="/"><img className="header-logo" src={Logo} alt="Trouve ton artisan avec la région Auvergne-Rhône-Alpes" /></Link>
+                </div>
+                <div className="nav-items">
+                    <div className="nav-search-container" ref={searchRef}>
+                        <div className="nav-search-logo">    
+                            <button className="nav-btn-search" onClick={()=>{setShowSearch(!showSearch)}}>
+                                <i className="material-symbols-outlined nav-logo">search</i>
+                            </button>
+                        </div>
+                        <div className={`nav-search ${showSearch ? "show-search" : "hide-search"}`}>
+                            <input type="text" className="nav-search-input" placeholder="Rechercher" />
+                            <i className="material-symbols-outlined nav-logo-search">search</i>
+                        </div>
+                    </div>
+                    <div className="nav-menu-container" ref={menuRef}>
+                        <div className="nav-burger-logo">
+                            <button className="nav-btn-burger" onClick={()=>{setShowLinks(!showLinks)}}>
+                                <i className="material-symbols-outlined nav-logo">menu</i> 
+                            </button>
+                        </div>
+                        <div className={`nav-menu ${showLinks ? "show-links" : "hide-links"}`}>
+                            <ul className="nav-menu-items">
+                                <li className="nav-menu-item"><Link to="building" className="nav-menu-link">Bâtiment</Link></li>
+                                <li className="nav-menu-item"><Link to="service" className="nav-menu-link">Services</Link></li>
+                                <li className="nav-menu-item"><Link to="fabrication" className="nav-menu-link">Fabrication</Link></li>
+                                <li className="nav-menu-item"><Link to="feeding" className="nav-menu-link">Alimentation</Link></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
