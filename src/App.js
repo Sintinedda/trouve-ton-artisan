d2bdfc774bc273cdbe1893781963eca19e9c2bc9
Jeremy Aboudan <aboudan.jeremy@gmail.com>
2024-08-14 15:10:46 +0200

    home

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 
 import Header from './Components/Header';
+import Home from './Components/Home';
 
 function App() {
   return ( 
     <div className="App">
       <Header />
+      <Routes>
+        <Route path="/">
+          <Route index element={<Navigate to="home" replace />} />
+          <Route path="home" element={<Home/>} />
+        </Route>
+      </Routes>
     </div>
   );
 }
